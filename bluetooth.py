c03b5da89aa0e3b6d7303ad7c9a7def8333b04e0
bsbrabrand <bsbrabrand@yahoo.com>
2025-04-05 22:45:15 -0400

    created file for bluetooth

---
+++ bluetooth.py
@@ -0,0 +1,27 @@
+class monitor:
+    def __init__():
+        import bluetooth
+        import time
+        # Replace with the actual address of your heart rate monitor
+        address = "00:00:00:00:00:00"
+
+        # Create a socket
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        try:
+            # Connect to the heart rate monitor
+            sock.connect((address, 1))
+
+            
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
+    def get_heart_rate(self):
+        # Read data from the socket
+        data = self.sock.recv(1024)
+        return data
+
+    def close(self):
+            # Close the socket
+            self.sock.close()
